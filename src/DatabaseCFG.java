ce28bd1ed95555f11711ccf8477ee985b0e00505
John Eleanor <mrs.ch64@gmail.com>
2024-03-12 11:21:02 +0700

    .

--- src/DatabaseCFG.java
+++ src/DatabaseCFG.java
@@ -3,11 +3,12 @@ import java.sql.*;
 import javax.swing.JOptionPane;
 
 public class DatabaseCFG {
+    private String databaseName = "car_rental_system";
 
-    private String url = "jdbc:mysql://localhost:3306/car_rental_system";
+    private String url = "jdbc:mysql://localhost:3306/"+this.databaseName;
     private String user = "root";
     private String password = "";
-    private String databaseName = "car_rental_system";
+    
 
     public void init() {
         try (Connection conn = DriverManager.getConnection(this.url, this.user, this.password);
@@ -32,44 +33,51 @@ public class DatabaseCFG {
     public boolean checkUser(String username, String password, boolean isLogin) {
 
         try (Connection conn = DriverManager.getConnection(this.url, this.user, this.password);
+
                 Statement stmt = conn.createStatement();
                 ResultSet rs = stmt.executeQuery(
                         "SELECT * FROM user WHERE username = '" + username + "' AND password = '" + password + "'")) {
-            if (rs.next()) {
+            if (conn.isClosed()) {
+                System.out.println("[Debug]: Connection Closed");
+            } else {
+                if (rs.next()) {
 
-                if (isLogin) {
+                    if (isLogin) {
 
-                    System.out.println("[Debug]: isLogin True");
-                    return true;
+                        System.out.println("[Debug]: isLogin True");
+                        return true;
+                    } else {
+
+                        System.out.println("[Debug]: User Found");
+                        return false;
+                    }
+
                 } else {
-
-                    System.out.println("[Debug]: User Found");
+                    System.out.println("[Debug]: User Not Found");
                     return false;
                 }
 
-            } else {
-                System.out.println("[Debug]: User Not Found");
-                return false;
             }
-
         } catch (SQLException e) {
             System.out.println(e.getMessage());
 
         }
+
         return false;
     }
 
     public boolean insertUser(String username, String password, String email) {
         try (Connection conn = DriverManager.getConnection(this.url, this.user, this.password);
                 Statement stmt = conn.createStatement()) {
-                String sql = "INSERT INTO user (username, password, role, email) VALUES ('" + username + "', '" + password + "', 'user', '" + email + "')";
+            String sql = "INSERT INTO user (username, password, role, email) VALUES ('" + username + "', '" + password
+                    + "', 'user', '" + email + "')";
             stmt.executeUpdate(sql);
 
             if (checkUser(username, password, false)) {
                 System.out.println("[Debug]: User Insert Successfully");
                 return true;
-            }else {
-                
+            } else {
+
                 System.out.println("[Debug]: User Have Already");
                 return false;
             }
